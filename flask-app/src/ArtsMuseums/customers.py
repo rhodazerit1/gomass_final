a348c52096c93775e8ffda79fce9fc682e46fa67
melinayg <yang.meli@northeastern.edu>
2024-04-11 17:39:16 -0400

    routes folders

--- flask-app/src/ArtsMuseums/customers.py
+++ flask-app/src/ArtsMuseums/customers.py
@@ -9,11 +9,27 @@ from src import db
 
 customers = Blueprint('customers', __name__)
 
+# Update User information for user
+@customers.route('/customers', methods=['PUT'])
+def update_customer():
+    cus_info = request.json
+    cus_id = cus_info['id']
+    first = cus_info['first_name']
+    last = cus_info['last_name']
+    company = cus_info['company']
+
+    query = 'UPDATE customers SET first_name = %s, last_name = %s, company = %s where id = %s'
+    data = (first, last, company, cus_id)
+    cursor = db.get_db().cursor()
+    x = cursor.execute(query, data)
+    db.get_db().commit()
+    return 'Customer info updated.'
+
 # Get all customers from the DB
 @customers.route('/customers', methods=['GET'])
 def get_customers():
     cursor = db.get_db().cursor()
-    cursor.execute('select company, last_name,\
+    cursor.execute('select id, company, last_name,\
         first_name, job_title, business_phone from customers')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
@@ -39,3 +55,5 @@ def get_customer(userID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+
