5d682df71c4a3291d22b4af61b7f236ed0e46f9e
melinayg <yang.meli@northeastern.edu>
2024-04-11 18:01:45 -0400

    updated post route in user folder?
    check which post route to use

--- flask-app/src/User/customers.py
+++ flask-app/src/User/customers.py
@@ -108,3 +108,31 @@ def add_customer():
     }
     
     return jsonify(response_data), 201
+
+@users.route('/product', methods=['POST'])
+def add_new_product():
+    
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    name = the_data['product_name']
+    description = the_data['product_description']
+    price = the_data['product_price']
+    category = the_data['product_category']
+
+    # Constructing the query
+    query = 'insert into products (product_name, description, category, list_price) values ("'
+    query += name + '", "'
+    query += description + '", "'
+    query += category + '", '
+    query += str(price) + ')'
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    return 'Success!'
