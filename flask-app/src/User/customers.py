0e5a0b81b6064460515a21cc30df8e0e1140fb4b
melinayg <yang.meli@northeastern.edu>
2024-04-11 17:56:48 -0400

    updated routes

--- flask-app/src/User/customers.py
+++ flask-app/src/User/customers.py
@@ -56,4 +56,55 @@ def get_customer(userID):
     the_response.mimetype = 'application/json'
     return the_response
 
+# Delete User from DB
+@customers.route('/customers/<userID>', methods=['DELETE'])
+def delete_user(userID):
+    cursor = db.get_db().cursor()
+    cursor.execute('DELETE FROM customers WHERE id = %s', (userID,))
+    db.get_db().commit()
+    return 'Customer deleted.', 200
 
+"""
+@customers.route('/customers/<userID>', methods=['DELETE'])
+def delete_customer(userID):
+    cursor = db.get_db().cursor()
+
+    # Construct the SQL query for deleting the customer by ID
+    query = 'DELETE FROM customers WHERE id = %s'
+    data = (userID,)
+
+    # Execute the SQL command and commit to the database
+    cursor.execute(query, data)
+    db.get_db().commit()
+    
+    # Check if the customer was deleted
+    if cursor.rowcount == 0:
+        # No customer was deleted, possibly because the customer did not exist
+        return jsonify(message="Customer not found"), 404
+    else:
+        # Respond with success message and status code
+        return jsonify(message="Customer deleted successfully"), 200
+"""
+
+# Add a new customer to the DB
+@customers.route('/customers', methods=['POST'])
+def add_customer():
+    cus_info = request.json
+    first = cus_info['first_name']
+    last = cus_info['last_name']
+    company = cus_info['company']
+
+    query = 'INSERT INTO customers (first_name, last_name, company) VALUES (%s, %s, %s)'
+    data = (first, last, company)
+    cursor = db.get_db().cursor()
+    cursor.execute(query, data)
+    db.get_db().commit()
+    
+    new_customer_id = cursor.lastrowid
+    
+    response_data = {
+        'message': 'Customer added successfully.',
+        'customer_id': new_customer_id
+    }
+    
+    return jsonify(response_data), 201
